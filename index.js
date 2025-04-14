8b6e2d07fa7151bf2492d08a588f01a6f947e76a
ibi <ibifran84@gmail.com>
2025-04-14 18:41:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
-function cifrarIbi(texto, desplazamiento) {
-  return texto.split('').map(char => {
-    let c = char.charCodeAt(0);
-    if (c >= 65 && c <= 90) {
-      return String.fromCharCode(((c - 65 + desplazamiento) % 26) + 65);
-    } else if (c >= 97 && c <= 122) {
-      return String.fromCharCode(((c - 97 + desplazamiento) % 26) + 97);
-    } else {
-      return char;
-    }
-  }).join('');
-}
 
-document.getElementById("texto").addEventListener("input", actualizar);
-document.getElementById("desplazamiento").addEventListener("input", actualizar);
 
-function actualizar() {
-  const texto = document.getElementById("texto").value;
-  const desplazamiento = parseInt(document.getElementById("desplazamiento").value);
-  const resultado = cifrarIbi(texto, desplazamiento);
-  document.getElementById("resultado").innerText = resultado;
-}
